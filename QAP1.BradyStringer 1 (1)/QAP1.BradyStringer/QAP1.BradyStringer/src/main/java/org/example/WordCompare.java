6c8d9bf14d2295d4701ac0825911b751e471771a
Bradystringer <95232247+Bradystringer@users.noreply.github.com>
2022-11-02 02:23:19 -0700

    Delete QAP1.BradyStringer 1 (1)/QAP1.BradyStringer directory

--- QAP1.BradyStringer 1 (1)/QAP1.BradyStringer/QAP1.BradyStringer/src/main/java/org/example/WordCompare.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-public class WordCompare {
-    public boolean findWordCompare(String wordCompare, String charSequence) {
-        return charSequence.contains(wordCompare);
-    }
-}
-
